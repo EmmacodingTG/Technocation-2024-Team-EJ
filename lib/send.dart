d804c2f7f119aa9484c22e5ec1dc4ccd93f4eda4
Jay Kuo <jaykuo2020@gmail.com>
2024-04-06 15:15:35 -0400

    This is the dates part.

--- lib/send.dart
+++ lib/send.dart
@@ -2,7 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 
 import 'package:flutter/material.dart';
 import 'package:litter_app/find_location_page.dart';
-
+import 'package:litter_app/signup_page.dart';
 import 'package:litter_app/stopwatch_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'clean_page.dart';
@@ -33,23 +33,17 @@ class _SendPageState extends State<SendPage> {
   @override
   Widget build(BuildContext context) {
     var subject = '';
-    var message = "Hello, this is a message about " +
-        usernameControllertext +
-        " 's cleaning session."; // __ replaced by textediting controller variable of username from login or sign up page
-    var message2 = 'Address:';
-    var message3 = 'Cleaning date: ';
+    var message = "Hello,\n\n"
+        "This is a message about $username's cleaning session."; // __ replaced by textediting controller variable of username from login or sign up page
+    var message2 = 'Address: $user_location';
+    var message3 = 'Cleaning date: $user_date';
     var message4 = 'Total cleaning time: $user_time';
-    var message5 =
-        'The before and after images were sent earlier to your inbox.';
-    var message6 = 'Please reach out to ' +
-        usernameControllertext +
-        ' if there are any issues.\n\nThank you!\n\n From: Greener Parks';
+    var message5 = 'The before and after images were sent earlier to your inbox.';
+    var message6 = 'Please reach out to $username if there are any issues.\n\nThank you!\n\nFrom: Greener Parks';
 
     // create global variable of timer and contacts ( put contacts email into the recipient's email )
 
     var Enter = '\n\n';
-
-    String DateTimeString = date;
 
     String messageperiode = message + Enter;
 
@@ -58,7 +52,6 @@ class _SendPageState extends State<SendPage> {
         address +
         Enter +
         message3 +
-        DateTimeString +
         Enter +
         message4 +
         Enter +
@@ -125,14 +118,12 @@ class _SendPageState extends State<SendPage> {
                         .split("{'after_image'")
                         .length;
                   }
-                  DatabaseReference records_list = FirebaseDatabase.instance
-                      .ref('users_list/$user_index/records');
-                  DatabaseReference new_record =
-                      records_list.child('${length_records + 1}');
+                  DatabaseReference records_list = FirebaseDatabase.instance.ref('users_list/$user_index/records');
+                  DatabaseReference new_record = records_list.child('${length_records + 1}');
                   new_record.set({
                     'after_image': user_after_image,
                     'before_image': user_before_image,
-                    'date': 'this is where you add the user_date variable',
+                    'date': user_date,
                     'location': user_location,
                     'volunteered_time': user_time,
                   });
